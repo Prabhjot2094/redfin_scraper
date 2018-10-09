7dd6c757e2cb2e5df2127af8455a4296f7c9d560
Prabhjot Singh <prabhjotsingh@Prabhjots-MacBook-Pro.local>
2018-10-09 12:45:02 -0700

    Data not written to db

--- redfin_scraper/redfin_scraper/settings.py
+++ redfin_scraper/redfin_scraper/settings.py
@@ -88,7 +88,7 @@ CONCURRENT_REQUESTS_PER_IP = 2
 # Configure item pipelines
 # See https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
-    'redfin_scraper.pipelines.RedfinScraperDBPipeline': 200,
+    #'redfin_scraper.pipelines.RedfinScraperDBPipeline': 200,
     #'redfin_scraper.pipelines.RedfinScraperPipeline': 300,
 }
 
--- redfin_scraper/setup.py
+++ redfin_scraper/setup.py
@@ -1,11 +1,13 @@
 import os
 
 polipo_config = ['socksParentProxy = localhost:9050', 'diskCacheRoot=""', 'disableLocalInterface=true']
+scrapy_dependencies = "sudo apt-get install python-dev python-pip libxml2-dev libxslt1-dev zlib1g-dev libffi-dev libssl-dev"
 
 def install_linux():
     os.system('sudo apt-get update')
     os.system('sudo apt-get install tor')
     os.system('sudo apt-get install polipo')
+    os.system(scrapy_dependencies)
 
     f = open ("/etc/polipo/config","w")
     for setting in polipo_config:
